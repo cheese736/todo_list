299e9d13f1b1f9c0a55aa4a1f527c54b9d09ecff
Aither <cheese736@hotmail.com>
2022-10-06 21:42:01 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -35,11 +35,10 @@ app.get('/todos/new', (req,res) => {
 })
 
 app.post('/todos', (req,res) => {
-  console.log('req.body', req.body)
-  // const name = req.body.name
-  // Todo.create({name})
-  // .then(() => res.redirect('/'))
-  // .catch(error => console.log(error))
+  const name = req.body.name
+  Todo.create({name})
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
 })
 
 app.get('/todos/:id', (req, res) => {
@@ -47,6 +46,34 @@ app.get('/todos/:id', (req, res) => {
   return Todo.findById(id)
     .lean()
     .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(()=> res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
