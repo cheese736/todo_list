823196ce33d5994a87258e22db5f9df5c1f6ff64
Aither <cheese736@hotmail.com>
2022-10-06 11:19:35 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+const exphbs = require('express-handlebars')
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 
@@ -18,8 +22,13 @@ db.once('open', () => {
 
 
 // 設定首頁路由
+const Todo = require('./models/todo') // 載入 Todo model
+// ...
 app.get('/', (req, res) => {
-  res.send('hello world')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // 設定 port 3000
