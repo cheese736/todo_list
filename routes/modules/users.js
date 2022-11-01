f9f9c84ab941c1bbc618e058838fc1aca51e4ca8
Aither <cheese736@hotmail.com>
2022-11-01 18:18:08 +0800

    feat: add user modules

---
+++ routes/modules/users.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const Todo = require('../../models/todo')
+
+router.get('/login', (req,res) => {
+  res.render('login')
+})
+
+module.exports = router
