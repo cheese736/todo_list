e0b21360685baeae195a56f1a9c2fda417e5ad5f
Aither <cheese736@hotmail.com>
2022-11-02 23:15:11 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
 const methodOverride = require('method-override')
 
 const routes = require('./routes') //automatically load index.js file
@@ -21,6 +23,8 @@ app.use(session({
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 // setting routes
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 const Todo = require('../../models/todo')
 const User = require('../../models/user')
+
 
 router.get('/login', (req,res) => {
   res.render('login')
 })
 
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
 router.get('/register', (req,res) => {
   res.render('register')
 })
+
 
 router.post('/register', (req,res) => {
   const {
