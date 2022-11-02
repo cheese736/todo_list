4b807191c6ac568b5a79bda46c094d56b4228e18
Aither <cheese736@hotmail.com>
2022-11-02 19:18:09 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Todo = require('../../models/todo')
+const User = require('../../models/user')
 
 router.get('/login', (req,res) => {
   res.render('login')
@@ -9,4 +10,34 @@ router.get('/login', (req,res) => {
 router.get('/register', (req,res) => {
   res.render('register')
 })
+
+router.post('/register', (req,res) => {
+  const {
+    name, email, password, confirmPassword
+  } = req.body
+  User.findOne({email})
+  .then(user => {
+    if (user) {
+      // 檢查email是否已註冊，若是，回到填寫頁面，保持輸入資訊
+      console.log('Operation failed: email has been used.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+      // 若否，寫入資料庫
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+    }
+  })
+  .catch(err => console.log(err))
+})
+
 module.exports = router
