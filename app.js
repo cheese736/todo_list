a22a2bc46b81fb9aeb636105e441606357195396
Aither <cheese736@hotmail.com>
2022-11-02 20:06:31 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const methodOverride = require('method-override')
 
 const routes = require('./routes') //automatically load index.js file
@@ -10,6 +11,13 @@ const app = express()
 // set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: "ThisIsMySecret",
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
