5891497c09837a19b26b9d99322b5ca5ba6cd2e4
Aither <cheese736@hotmail.com>
2022-11-03 00:49:09 +0800

    feat: add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,8 @@ const Todo = require('../../models/todo')
 
 // 設定首頁路由
 router.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
+  const userId = req.user._id
+  Todo.find({userId}) 
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .sort({name: 'asc'})
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
