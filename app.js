62fe36ce746fabbabcdea9899568bc86cf62a808
Aither <cheese736@hotmail.com>
2022-11-03 20:02:55 +0800

    feat add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const session = require('express-session')
 // 載入設定檔，要寫在 express-session 以後
 const usePassport = require('./config/passport')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes') //automatically load index.js file
 require('./config/mogoose')
@@ -13,6 +14,7 @@ const app = express()
 // set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 
 app.use(session({
   secret: "ThisIsMySecret",
@@ -27,11 +29,14 @@ app.use(methodOverride('_method'))
 
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
+app.use(flash())
 
 app.use((req, res, next) => {
   console.log(req.user)
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -23,12 +23,35 @@ router.post('/register', (req,res) => {
   const {
     name, email, password, confirmPassword
   } = req.body
+  const errors = []
+
+  if ( [name,email,password,confirmPassword].includes('')) {
+    errors.push({message: '所有欄位都是必填。'})
+  }
+
+  if (password !== confirmPassword) {
+    errors.push({message: '密碼與確認密碼不相符'})
+  }
+
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+    
+  }
+
   User.findOne({email})
   .then(user => {
     if (user) {
+      errors.push({message: '此email已註冊'})
       // 檢查email是否已註冊，若是，回到填寫頁面，保持輸入資訊
       console.log('Operation failed: email has been used.')
-      res.render('register', {
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -50,6 +73,7 @@ router.post('/register', (req,res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出')
   res.redirect('/users/login')
 })
 
