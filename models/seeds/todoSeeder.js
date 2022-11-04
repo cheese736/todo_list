5cd47fd0cd6c2d4e5b4626318374f8743a086933
Aither <cheese736@hotmail.com>
2022-11-05 02:27:20 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,18 +1,36 @@
-const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const bcrypt = require('bcryptjs')
 const Todo = require('../todo.js') // 載入 todo model
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mogoose')
+const User = require('../user')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  Todo.deleteMany({}).then(() => console.log('clear data'))
-  .catch((err) => console.log(err))
-  for (let i = 0; i < 10; i++) {
-    Todo.create({name: `name-${i}`, isDone: false})
-  }
+  bcrypt
+  .genSalt(10)
+  .then(salt => bcrypt.hash(SEED_USER.password, salt))
+  .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    })
+  )
+  .then(user => {
+    console.log(user)
+    const userId = user._id
+    for (let i = 0; i < 10; i++) {
+      Todo.create({name: `name-${i}`, userId})
+    }
+  })
+  .then(() => {
+    console.log('done')
+    process.exit()
+  })
 })
